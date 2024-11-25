7e13f21a7b59cfdd0bcfc7dc980c412696113575
Farseena-M <farseenamoyan19@gmail.com>
2024-11-25 19:48:43 +0530

    cors

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 import express from 'express'
+import cors from 'cors'
 import authRouter from './src/routers/authRouter.js'
 import accessRouter from './src/routers/accessRouter.js'
-const app= express()
+const app = express()
 app.use(express.json())
-
-app.use('/api',authRouter)
-app.use('/api',accessRouter)
+app.use(cors({
+    origin: 'http://localhost:5173', 
+    methods: ['GET', 'POST'],
+    credentials: true,
+}));
+app.use('/api', authRouter)
+app.use('/api', accessRouter)
 
 export default app
